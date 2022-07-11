3709ed1fe041df4acc37fba874e7de75217b723f
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-11 17:35:25 +0200

    lista spesa con ciclo while

---
+++ index.js
@@ -0,0 +1,21 @@
+const listaSpesa = ['pomodori',
+                    'uova',
+                    'latte',
+                    'carne',
+                    'formaggio',
+                    'salmone',
+                    'avocado',
+                    'cereali',
+                    'scottex',
+                    'olive'
+]
+
+let i = 0;
+
+while (i < listaSpesa.length ) {
+
+    console.log (listaSpesa[i]);
+
+
+    i++;
+}
